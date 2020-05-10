1f45565098b43efee4acf7cad5630b231f6a20cc
jgrindall <john.grindall@gmail.com>
2020-05-10 16:07:42 +0100

    add compoaraotr

--- src/PointComparator.ts
+++ src/PointComparator.ts
@@ -9,3 +9,13 @@ export const compX:PointComparator = (a:Point, b:Point):number => {
 export const compY:PointComparator = (a:Point, b:Point):number => {
     return a[1] > b[1] ? 1 : (a[1] < b[1] ? -1 : 0);
 };
+
+export const getCompAngle = (centre:Point):PointComparator=>{
+    const angle = (p:Point):number =>{
+        return Math.atan2(p[1] - centre[1], p[0] - centre[0]);
+    };
+    return (a:Point, b:Point):number => {
+        const angleA:number = angle(a), angleB:number = angle(b);
+        return angleA < angleB ? 1 : (angleA > angleB ? -1 : 0);
+    };
+};
