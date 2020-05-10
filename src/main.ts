1dcd4c1f0582cdc6586a97a7efe3e52a8d6bf5f2
jgrindall <john.grindall@gmail.com>
2020-05-10 18:35:29 +0100

    add interactivity

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ const sideLength:number = 150;
 
 const RT3:number = Math.sqrt(3);
 
-const ctx = (document.getElementById("canvas") as HTMLCanvasElement).getContext("2d");
+const canvas = (document.getElementById("canvas") as HTMLCanvasElement);
+const ctx = canvas.getContext("2d");
+
+const clear = ()=>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+};
 
 const drawPoly = (p:Polygon, strokeStyle:string, fillStyle:string)=>{
     ctx.fillStyle = fillStyle;
@@ -31,17 +36,40 @@ const fundamentalPolygon: Polygon = [
 
 const g: WallpaperGroup = Wallpaper.generateGroup(Wallpaper.P3M1, fundamentalPolygon);
 
-const screen:Rect = [
-    [300, 320],
-    [300, 620],
-    [800, 620],
-    [800, 320]
-];
+const redraw = (x:number, y:number):void=>{
+    const screen:Rect = [
+        [x, y],
+        [x, y + 300],
+        [x + 500, y + 300],
+        [x + 500, y]
+    ];
+    clear();
+    drawPoly(screen, 'rgba(100, 100, 100, 1)', 'rgba(100, 100, 100, 0.5)');
+    drawPoly(fundamentalPolygon, 'rgba(200, 0, 0, 1)', 'rgba(200, 0, 0, 0.5)');
+    const ts:Array<PolygonTransform> = g.coverRectangle(screen);
 
-drawPoly(screen, 'rgba(100, 100, 100, 1)', 'rgba(100, 100, 100, 0.5)');
-drawPoly(fundamentalPolygon, 'rgba(200, 0, 0, 1)', 'rgba(200, 0, 0, 0.5)');
-const ts:Array<PolygonTransform> = g.coverRectangle(screen);
+    ts.forEach(t=>{
+        drawPoly(t.poly1, 'rgba(50, 200, 50, 1)', 'rgba(50, 200, 50, 0.5)');
+    });
 
-ts.forEach(t=>{
-    drawPoly(t.poly1, 'rgba(50, 200, 50, 1)', 'rgba(50, 200, 50, 0.5)');
-});
+}
+
+let dragging:boolean = false;
+
+const handleMouseDown = (event: MouseEvent) => {
+    dragging = true;
+};
+
+const handleMouseUp = (event: MouseEvent) => {
+    dragging = false;
+};
+
+const handleMouseMove = (event: MouseEvent) => {
+    if(dragging){
+        redraw(event.clientX, event.clientY);
+    }
+};
+
+document.addEventListener('mousemove', handleMouseDown);
+document.addEventListener('mouseup', handleMouseUp);
+document.addEventListener('mousemove', handleMouseMove);
