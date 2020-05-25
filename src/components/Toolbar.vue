27d710433af9445c45d722ecb3fa87c14b2c14e8
jgrindall <john.grindall@gmail.com>
2020-05-25 21:09:29 +0100

    components

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <md-toolbar class="md-primary">
+      <div class="md-toolbar-row">
+        <div class="md-toolbar-section-start">
+          <md-button class="md-icon-button">
+            <md-icon>menu</md-icon>
+          </md-button>
+        </div>
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button">
+            <md-icon>refresh</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>more_vert</md-icon>
+          </md-button>
+        </div>
+      </div>
+    </md-toolbar>
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+      name: 'ContentActions'
+    }
+</script>
